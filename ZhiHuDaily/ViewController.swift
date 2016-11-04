4ac550daddd66046c9e279f9f327d1383a4bb1b4
Rabbbbbbit <aaaasd_66589559@qq.com>
2016-11-04 15:43:20 +0800

    baa

--- ZhiHuDaily/ViewController.swift
+++ ZhiHuDaily/ViewController.swift
@@ -9,16 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    let name : String = nil
+    
+    git dif
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viegitwDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
